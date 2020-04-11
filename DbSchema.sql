32cb4ad8ff64ca9ab0380f612961fe5e65fd8458
Sam-Ballantyne <25254611+Sam-Ballantyne@users.noreply.github.com>
2020-04-11 20:41:07 +0100

    Added SQL files

---
+++ DbSchema.sql
@@ -0,0 +1,24 @@
+﻿USE [AudioDatabase]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[AudioFiles](
+	[Id] [uniqueidentifier] NOT NULL,
+	[FileName] [nchar](50) NOT NULL,
+	[Author] [uniqueidentifier] NOT NULL,
+	[FilePath] [varchar](300) NOT NULL,
+	[LastUpdated] [datetime] NOT NULL,
+ CONSTRAINT [Id_Index] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[AudioFiles] ADD  CONSTRAINT [DF_AudioFiles_LastUpdated]  DEFAULT (getdate()) FOR [LastUpdated]
+GO
---
+++ StoredProcs.sql
@@ -0,0 +1,54 @@
+﻿USE [AudioDatabase]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[AddAudioFile]
+	@Id uniqueidentifier,
+	@FileName nchar(50),
+	@AuthorId uniqueidentifier,
+	@FilePath varchar(300)
+AS
+BEGIN
+	SET NOCOUNT OFF;
+
+	INSERT INTO [AudioFiles]
+	(Id, FileName, Author, FilePath, LastUpdated)
+	VALUES
+	(@Id, @FileName, @AuthorId, @FilePath, GETUTCDATE())
+END
+
+GO
+
+CREATE PROCEDURE [dbo].[DeleteAudioFile]
+	@Id uniqueidentifier
+AS
+BEGIN
+	SET NOCOUNT OFF;
+
+	DELETE FROM [AudioFiles]
+	WHERE Id = @Id
+END
+
+GO
+
+CREATE PROCEDURE [dbo].[GetAudioFile]
+	@Id uniqueidentifier
+AS
+BEGIN
+	SET NOCOUNT OFF;
+
+	SELECT [Id]
+		  ,[FileName]
+		  ,[Author]
+		  ,[FilePath]
+		  ,[LastUpdated]
+	  FROM [AudioDatabase].[dbo].[AudioFiles]
+	  WHERE [Id] = @Id
+END
+
+GO
